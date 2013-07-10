6f2a62586859656fc34563d2e8eb8b7d687bfa2d
Simon Waldherr <git@simon.waldherr.eu>
2013-07-11 01:46:46 +0200

    jslint valid

--- static/shownotes_admin.js
+++ static/shownotes_admin.js
@@ -10,7 +10,10 @@
  * Version: 0.3.3
  */
 
+/*global majaX, shownotesname */
+
 function importShownotes(textarea, importid, baseurl) {
+  "use strict";
   var requrl;
   requrl = baseurl.replace("$$$", importid);
   majaX({url: requrl}, function (resp) {
@@ -19,72 +22,62 @@ function importShownotes(textarea, importid, baseurl) {
 }
 
 function getPadList(select, podcastname) {
-  var requrl, padslist, returnstring = '';
-  if(podcastname.trim() == "*") {
+  "use strict";
+  var requrl, padslist, returnstring = '', i;
+  if (podcastname.trim() === "*") {
     requrl = 'http://cdn.simon.waldherr.eu/projects/showpad-api/getList/';
   } else {
-    requrl = 'http://cdn.simon.waldherr.eu/projects/showpad-api/getList/?search='+podcastname.trim();
+    requrl = 'http://cdn.simon.waldherr.eu/projects/showpad-api/getList/?search=' + podcastname.trim();
   }
-  
+
   majaX({url: requrl, type: 'json'}, function (resp) {
     padslist = resp;
-    for(var i = 0; i < padslist.length; i++) {
+    for (i = 0; i < padslist.length; i += 1) {
       if (shownotesname === padslist[i].docname) {
-        returnstring += '<option selected>'+padslist[i].docname+'</option>';
+        returnstring += '<option selected>' + padslist[i].docname + '</option>';
       } else {
-        returnstring += '<option>'+padslist[i].docname+'</option>';
+        returnstring += '<option>' + padslist[i].docname + '</option>';
       }
     }
     select.innerHTML = returnstring;
   });
 }
 
-function templateAssociated (change) {
-  var delimiterele, lastdelimiterele, chapterdelimiterele, cssele, i, j;
+function templateAssociated(change) {
+  "use strict";
+  var delimiterele, lastdelimiterele;
   delimiterele = document.getElementById('main_delimiter');
   lastdelimiterele = document.getElementById('main_last_delimiter');
   document.getElementById('main_md_shortcode').parentNode.parentNode.style.display = 'none';
-  if(document.getElementById('main_mode').value == 'block style') {
+  if (document.getElementById('main_mode').value === 'block style') {
     delimiterele.parentNode.parentNode.style.display = 'table-row';
     lastdelimiterele.parentNode.parentNode.style.display = 'table-row';
-  } else if(document.getElementById('main_mode').value == 'button style') {
+  } else if (document.getElementById('main_mode').value === 'button style') {
     delimiterele.parentNode.parentNode.style.display = 'none';
     lastdelimiterele.parentNode.parentNode.style.display = 'none';
   } else {
     delimiterele.parentNode.parentNode.style.display = 'none';
     lastdelimiterele.parentNode.parentNode.style.display = 'none';
   }
-  if(change === 1) {
-    if(document.getElementById('main_mode').value == 'button style') {
+  if (change === 1) {
+    if (document.getElementById('main_mode').value === 'button style') {
       document.getElementById('css_id').value = 3;
     }
   }
 }
 
-function previewPopup (shownotesElement, emode, forceDL, apiurl) {
-  var shownotes = '', action;
-  //if((mode === 'html')||(mode === 'source')) {
-  //  shownotes = osfExport(osfParser(shownotesElement.value),osfExportModules['html']);
-  //} else if(mode === 'md') {
-  //  shownotes = osfExport(osfParser(shownotesElement.value),osfExportModules[mode]);
-  //} else if(mode === 'wikigeeks') {
-  //  shownotes = osfExport(osfParser(shownotesElement.value),osfExportModules[mode]);
-  //} else if(mode === 'chapter') {
-  //  shownotes = '<code style="white-space: pre;">'+osfExport(osfParser(shownotesElement.value),osfExportModules[mode])+'</code>';
-  //} else if(mode === 'glossary') {
-  //  shownotes = osfExport(osfParser(shownotesElement.value),osfExportModules[mode]);
-  //}
-
+function previewPopup(shownotesElement, emode, forceDL, apiurl) {
+  "use strict";
+  var action, shownotesPopup;
   if (forceDL === true) {
     forceDL = 'true';
   }
-  
-  action = document.forms["post"].action;
-  console.log(action);
-  return false;
-  document.forms["post"].action = apiurl;
-  
-  majaX({url:apiurl,method:'POST',data:{fdl:forceDL,mode:emode,shownotes:encodeURIComponent(shownotesElement.value)}}, function (resp) {
+
+  action = document.forms.post.action;
+  //return false;
+  //document.forms["post"].action = apiurl;
+
+  majaX({url: apiurl, method: 'POST', data: {fdl: forceDL, mode: emode, shownotes: encodeURIComponent(shownotesElement.value)}}, function (resp) {
     if (forceDL !== 'true') {
       shownotesPopup = window.open('', "Shownotes Preview", "width=400,height=300,resizable=yes");
       shownotesPopup.document.write(resp);
@@ -92,6 +85,6 @@ function previewPopup (shownotesElement, emode, forceDL, apiurl) {
       shownotesPopup.focus();
     }
   });
-  document.forms["post"].action = action;
-  //return false;
+  document.forms.post.action = action;
+  return false;
 }
