0c9260060600fde1fde441cf21fc6f4967f29c43
Simon Waldherr <git@simon.waldherr.eu>
2014-01-15 22:07:38 +0100

    analyze via tinyOSF

--- static/shownotes_admin.js
+++ static/shownotes_admin.js
@@ -13,17 +13,6 @@
 /*jslint browser: true, indent: 2 */
 /*global majaX, shownotesname, tinyosf, osfExportModules */
 
-
-function importShownotes(textarea, importid, baseurl) {
-  "use strict";
-  var requrl;
-  requrl = baseurl.replace("$$$", importid);
-  majaX({url: requrl}, function (resp) {
-    textarea.value = resp.trim();
-    analyzeShownotes();
-  });
-}
-
 function getPadList(select, podcastname) {
   "use strict";
   var requrl,
@@ -53,28 +42,19 @@ function getPadList(select, podcastname) {
 function analyzeShownotes() {
   "use strict";
   var textarea = document.getElementById('shownotes'),
-    shownotes = textarea.value,
-    ready = false,
-    header = false,
-    compatible = false,
-    podcast = false,
-    episode = false,
-    title = false,
-    chapters = 0,
-    revision = 0,
-    helper;
-  helper = shownotes.split(/[\\\/]head(er)?/gmi)[0].trim();
-  revision = shownotes.match(/( \#r | \#revision | \#r\n| \#revision\n)/gmi);
-  revision = (revision === null) ? 0 : revision.length;
-  ready = /\n *ready: ?(false|no|not|nicht)/gmi.test(helper) ? false : (revision > 0) ? false : true;
-  podcast = /\n *podcast: ?\w{3,}/gmi.test(helper);
-  episode = /\n *episode: ?\d+/gmi.test(helper);
-  title = /\n *(episode)?tit(le|el): *[\w\d ]{5,}/gmi.test(helper);
+    status = tinyosf.Parser(tinyosf.htmldecode(textarea.innerHTML)).info;
 
-  header = (helper.length > 23) ? podcast ? episode ? title : false : false : false;
-  chapters = tinyosf.Export(tinyosf.Parser(tinyosf.htmldecode(shownotes)), osfExportModules.chapter).split("\n").length;
-  compatible = (chapters > 3) ? true : (tinyosf.Parser(tinyosf.htmldecode(shownotes)).length > 23) ? true : false;
-  document.getElementById('snstatus').innerHTML = '<b>Status: </b><span title="length: ' + helper.length + '\npodcast: ' + podcast + '\nepisode: ' + episode + '\ntitle: ' + title + '" style="color:' + ((header === true) ? 'green' : 'red') + ';">header</span>, ' + '<span title="count: ' + chapters + '" style="color:' + ((chapters > 3) ? 'green' : 'red') + ';">chapters</span>, ' + '<span style="color:' + ((compatible === true) ? 'green' : 'red') + ';">compatible</span>, ' + '<span title="revision items: ' + revision + '" style="color:' + ((ready === true) ? 'green' : 'red') + ';">ready</span>';
+  document.getElementById('snstatus').innerHTML = '<b>Status: </b><span title="podcast: ' + status.podcast + '\nepisode: ' + status.episode + '\ntitle: ' + status.title + '" style="color:' + ((status.header === true) ? 'green' : 'red') + ';">header</span>, ' + '<span title="count: ' + status.chapters + '" style="color:' + ((status.chapters > 3) ? 'green' : 'red') + ';">chapters</span>, ' + '<span style="color:' + ((status.compatible === true) ? 'green' : 'red') + ';">compatible</span>, ' + '<span title="revision items: ' + status.revision + '" style="color:' + ((status.ready === true) ? 'green' : 'red') + ';">ready</span>';
+}
+
+function importShownotes(textarea, importid, baseurl) {
+  "use strict";
+  var requrl;
+  requrl = baseurl.replace("$$$", importid);
+  majaX({url: requrl}, function (resp) {
+    textarea.value = resp.trim();
+    analyzeShownotes();
+  });
 }
 
 function templateAssociated(change) {
